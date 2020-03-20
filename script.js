66b371e389dbae37c1d3658f05f6e940d78160b8
katedemedchuk <demedchuk.kate@gmail.com>
2020-03-20 20:49:16 +0300

    feat: add form submit logic

--- script.js
+++ script.js
@@ -25,19 +25,34 @@ let portfolioNavClickListner = e =>{
         let rand = Math.floor( Math.random()*100 )
         e.style.order = rand        
     })
-
-
-    console.log(items)
-}
-
-let shuffle = e => {
-    console.log(e.currentTarget)
-    console.log(e.target)
 }
 
 let portfolioClickListner = e =>{
     Array.from(e.currentTarget.children).forEach( el => el.classList.remove("portfolio-active-border"))
     e.target.classList.add("portfolio-active-border");
+}
+
+let formSubmitListener = e => {
+    e.preventDefault()
+    document.getElementById("form-submit-message").classList.remove('hide')
+
+    let subject = String(document.getElementById("subject-input").value);
+    let description = String(document.getElementById("description-input").value);
+
+    subject === ''  ?
+                    document.getElementById("subject-show").innerHTML = "no subject"
+                    :
+                    document.getElementById("subject-show").innerHTML = String().concat("Subject : ", subject)
+    
+    description === ''  ?
+                            document.getElementById("description-show").innerHTML = "no description"
+                        :
+                            document.getElementById("description-show").innerHTML = String().concat("Description : ", description)                        
+}
+
+let okButtonListener = e => {
+    document.getElementById("form-submit-message").classList.add('hide')    
+    document.getElementById("submit-form").reset()
 }
 
 document.getElementById("header-naw-list").addEventListener("click", navClickListener);
@@ -46,4 +61,7 @@ document.getElementById("phone-h").addEventListener("click", phoneClickListener)
 document.getElementsByClassName("navContainer")[0].addEventListener("click", portfolioNavClickListner);
 
 document.getElementsByClassName("portfolio-container")[0].addEventListener("click", portfolioClickListner);
+document.getElementById("submit-form").addEventListener("submit", formSubmitListener);
+document.getElementById("OK-button").addEventListener("click", okButtonListener);
+
 
